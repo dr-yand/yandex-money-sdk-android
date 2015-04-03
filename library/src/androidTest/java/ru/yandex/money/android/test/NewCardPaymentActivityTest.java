92f62cf993e1bf3f99cb6439e21ddf3d580628a2
Slava Yasevich <vyasevich@yamoney.ru>
2015-04-03 18:37:45 +0300

    Implemented SavedCardPaymentActivityTest.

--- library/src/androidTest/java/ru/yandex/money/android/test/NewCardPaymentActivityTest.java
+++ library/src/androidTest/java/ru/yandex/money/android/test/NewCardPaymentActivityTest.java
@@ -1,24 +1,10 @@
 package ru.yandex.money.android.test;
 
 import android.test.suitebuilder.annotation.LargeTest;
-import android.view.View;
-
-import com.robotium.solo.Condition;
-import com.yandex.money.api.utils.MillisecondsIn;
-
-import ru.yandex.money.android.test.properties.LocalProperties;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static com.robotium.solo.By.id;
-import static ru.yandex.money.android.test.espresso.MoreViewActions.waitView;
-import static ru.yandex.money.android.test.espresso.MoreViewActions.waitVisibilityChange;
 
 /**
  * @author Slava Yasevich (vyasevich@yamoney.ru)
@@ -27,55 +13,11 @@ import static ru.yandex.money.android.test.espresso.MoreViewActions.waitVisibili
 public final class NewCardPaymentActivityTest extends PaymentActivityTest {
 
     public void test() {
-        final String cardNumberId = "cardNumber";
         // waiting for WebFragment to be attached to PaymentActivity
         solo().waitForFragmentById(R.id.ym_container);
         // check that there was no error
         onView(withId(R.id.ym_error_title)).check(doesNotExist());
 
-        // waiting for WebView to load CPS page
-        solo().waitForCondition(new Condition() {
-            @Override
-            public boolean isSatisfied() {
-                return !solo().getWebElements(id(cardNumberId)).isEmpty();
-            }
-        }, sleep(testProperties.getNetworkTimeout()));
-
-        // entering card details
-        LocalProperties.Card card = localProperties.getCard();
-        solo().enterTextInWebElement(id(cardNumberId), card.number);
-        solo().enterTextInWebElement(id("month"), card.month);
-        solo().enterTextInWebElement(id("year"), card.year);
-        solo().enterTextInWebElement(id("cardCvc"), card.csc);
-        solo().clickOnWebElement(id("mobile-cps_submit-button"));
-
-        // waiting for success payment, may require manual user input (3D Secure for instance)
-        onView(isRoot()).perform(waitView(testProperties.getManualTimeout(), R.id.ym_success));
-
-        // checking success page and trying to save a card
-        onView(withId(R.id.ym_comment))
-                .check(matches(withText(getString(R.string.ym_success_comment,
-                        localProperties.getAmount()))));
-        onView(withId(R.id.ym_description))
-                .check(matches(withText(R.string.ym_success_save_card_description)));
-        onView(withId(R.id.ym_save_card))
-                .check(matches(withText(R.string.ym_success_save_card)))
-                .perform(click());
-
-        // waiting for a card to be saved
-        onView(withId(R.id.ym_save_card))
-                .perform(waitVisibilityChange(testProperties.getNetworkTimeout(), View.GONE));
-
-        // when a card is saved
-        onView(withId(R.id.ym_success_marker))
-                .check(matches(isDisplayed()));
-    }
-
-    private int sleep(int seconds) {
-        return seconds * (int) MillisecondsIn.SECOND;
-    }
-
-    private String getString(int resId, Object... params) {
-        return getActivity().getString(resId, params);
+        payForCard();
     }
 }
--- library/src/androidTest/java/ru/yandex/money/android/test/PaymentActivityTest.java
+++ library/src/androidTest/java/ru/yandex/money/android/test/PaymentActivityTest.java
@@ -3,14 +3,28 @@ package ru.yandex.money.android.test;
 import android.app.Instrumentation;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
 
+import com.robotium.solo.Condition;
 import com.robotium.solo.Solo;
 import com.yandex.money.api.methods.params.PhoneParams;
+import com.yandex.money.api.utils.MillisecondsIn;
 
 import ru.yandex.money.android.PaymentActivity;
 import ru.yandex.money.android.PaymentArguments;
 import ru.yandex.money.android.test.properties.LocalProperties;
 import ru.yandex.money.android.test.properties.TestProperties;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.robotium.solo.By.id;
+import static ru.yandex.money.android.test.espresso.MoreViewActions.waitView;
+import static ru.yandex.money.android.test.espresso.MoreViewActions.waitVisibilityChange;
 
 /**
  * @author Slava Yasevich (vyasevich@yamoney.ru)
@@ -38,7 +52,9 @@ abstract class PaymentActivityTest extends ActivityInstrumentationTestCase2<Paym
         setActivityIntent(intent);
 
         Instrumentation instrumentation = getInstrumentation();
-        Cleaner.perform(instrumentation.getContext());
+        if (shouldPerformClean()) {
+            Cleaner.perform(instrumentation.getContext());
+        }
         solo = new Solo(instrumentation, getActivity());
     }
 
@@ -50,8 +66,60 @@ abstract class PaymentActivityTest extends ActivityInstrumentationTestCase2<Paym
         return localProperties.getClientId();
     }
 
+    protected boolean shouldPerformClean() {
+        return true;
+    }
+
+    protected void payForCard() {
+        final String cardNumberId = "cardNumber";
+        // waiting for WebView to load CPS page
+        solo().waitForCondition(new Condition() {
+            @Override
+            public boolean isSatisfied() {
+                return !solo().getWebElements(id(cardNumberId)).isEmpty();
+            }
+        }, sleep(testProperties.getNetworkTimeout()));
+
+        // entering card details
+        LocalProperties.Card card = localProperties.getCard();
+        solo().enterTextInWebElement(id(cardNumberId), card.number);
+        solo().enterTextInWebElement(id("month"), card.month);
+        solo().enterTextInWebElement(id("year"), card.year);
+        solo().enterTextInWebElement(id("cardCvc"), card.csc);
+        solo().clickOnWebElement(id("mobile-cps_submit-button"));
+
+        // waiting for success payment, may require manual user input (3D Secure for instance)
+        onView(isRoot()).perform(waitView(testProperties.getManualTimeout(), R.id.ym_success));
+
+        // checking success page and trying to save a card
+        onView(withId(R.id.ym_comment))
+                .check(matches(withText(getString(R.string.ym_success_comment,
+                        localProperties.getAmount()))));
+        onView(withId(R.id.ym_description))
+                .check(matches(withText(R.string.ym_success_save_card_description)));
+        onView(withId(R.id.ym_save_card))
+                .check(matches(withText(R.string.ym_success_save_card)))
+                .perform(click());
+
+        // waiting for a card to be saved
+        onView(withId(R.id.ym_save_card))
+                .perform(waitVisibilityChange(testProperties.getNetworkTimeout(), View.GONE));
+
+        // when a card is saved
+        onView(withId(R.id.ym_success_marker))
+                .check(matches(isDisplayed()));
+    }
+
     private PaymentArguments createArguments() {
         PhoneParams params = localProperties.getPhoneParams();
         return new PaymentArguments(getClientId(), params.getPatternId(), params.makeParams());
+    }
+
+    private int sleep(int seconds) {
+        return seconds * (int) MillisecondsIn.SECOND;
+    }
+
+    private String getString(int resId, Object... params) {
+        return getActivity().getString(resId, params);
     }
 }
